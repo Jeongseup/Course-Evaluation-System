b7059a7973254eb8edf99d22d90bacac00bf6e6d
Jeongseup <thswjdtmq4@inu.ac.kr>
2021-07-22 00:12:51 +0900

    Revert "[FINAL] 최종 수정 백업"
    This reverts commit 65ff50d5f2431b6b566986c82a890c9b16945292.

--- client/src/api.js
+++ client/src/api.js
@@ -4,9 +4,7 @@ axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8'
 axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*'
 
 export default {
-    mounted() {
-        this.$emit('change-path', this.$route.path)
-    },
+    mounted() {},
     methods: {
         async $api(url, method, data) {
             return (
