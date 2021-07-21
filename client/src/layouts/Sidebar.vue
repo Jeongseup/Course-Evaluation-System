102739a61473f64241f1ac0889857f1b6aff6eb5
tejin3 <86044250+tejin3@users.noreply.github.com>
2021-07-21 21:17:31 +0900

    [우선]업데이xm

--- client/src/layouts/Sidebar.vue
+++ client/src/layouts/Sidebar.vue
@@ -77,12 +77,6 @@
 </template>
 <script>
 export default {
-    methods: {
-        goToListPage() {
-            this.$router.push({
-                path: '/list'
-            })
-        }
-    }
+    methods: {}
 }
 </script>
